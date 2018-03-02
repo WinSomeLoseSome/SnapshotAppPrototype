5173a5938ce5ab2de50c9ccf0d88628975521e4d
bentonrobertson <benton.rob12@gmail.com>
2018-03-02 14:48:23 -0800

    Initial Commit

---
+++ Snapshot Protoype 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Snapshot Protoype 1
+//
+//  Created by Benton Robertson on 2018-03-02.
+//  Copyright © 2018 Snapshot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
